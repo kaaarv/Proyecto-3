b91b3036ef0661a290ec1345631df2db1584efdb
Randy <randysteve500@gmail.com>
2025-07-03 09:54:49 -0600

    actualizacion

--- Pipeline/And.sv
+++
@@ -1,9 +0,0 @@
-module and1 (
-    input wire A,
-    input wire B,
-    output wire Q
-);
-
-assign Q=A&B;
-
-endmodule
--- Pipeline/comparador.sv
+++
@@ -1,9 +0,0 @@
-module comparador (
-    input logic [63:0] dato_rs1,
-    input logic [63:0] dato_rs2,
-    output logic resultado
-);
-assign resultado = (dato_rs1==dato_rs2) ? 1'b1 : 1'b0;
-
-
-endmodule
